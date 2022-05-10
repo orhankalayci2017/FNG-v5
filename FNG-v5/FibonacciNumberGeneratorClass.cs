8f3bbf651c3990ef47bacb4c25dc3e68cf792361
orhankalayci2017 <orhan@kalayci.com>
2022-05-10 21:37:36 +0300

    setting upper limit for index to 50

--- FNG-v5/FibonacciNumberGeneratorClass.cs
+++ FNG-v5/FibonacciNumberGeneratorClass.cs
@@ -8,10 +8,7 @@ namespace FNG_v5
         {
         }
 
-        private bool checkIfIndexValid(int index)
-        {
-            return (index >= 0);
-        }
+        private bool checkIfIndexValid(int index) => ((index >= 0) && (index < 50));
 
         internal int errorCode()
         {
